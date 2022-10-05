31f81da85e286e7d64883717a7db5bcfc4c140ef
Rasmus Pagh <pagh@di.ku.dk>
2022-10-05 15:23:57 +0200

    Code for NeurIPS final version

--- private-count-sketch.py
+++ private-count-sketch.py
@@ -2,8 +2,12 @@
 
 import numpy as np
 from scipy.special import gammaln
+from scipy.sparse import coo_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
+import requests
+import gzip
+import os
 
 sns.set_theme()
 trials = 1000000
@@ -21,7 +25,7 @@ standard_gaussian_noise = np.random.normal(size=trials, scale=1)
 
 def quantile_gaussian_noise(trials, repetitions, quantile=0.5):
     gaussian_noise = np.random.normal(size=(trials, repetitions), scale=np.sqrt(repetitions))
-    return np.quantile(gaussian_noise, quantile, axis = 1) #, method='closest_observation')
+    return np.quantile(gaussian_noise, quantile, axis = 1)
 
 for repetitions in repetition_list:
     sns.kdeplot(quantile_gaussian_noise(trials, repetitions), label=f"k={repetitions}")
@@ -59,31 +63,6 @@ for repetitions in range(1,7,2):
     print(f"k={repetitions}: {average_abs_median_gaussian(trials, repetitions)}")
 
 
-# Added noise for median of k normals with mean zero and variance k
-
-count_sketch_error_probability = 1 # Probability of error
-count_sketch_error_value = 10000 # Value of error, otherwise zero
-
-plt.figure()
-for repetitions in repetition_list:
-    cs_errors = np.random.choice([-count_sketch_error_value, 0, count_sketch_error_value], p=[count_sketch_error_probability / 2, 1 - count_sketch_error_probability, count_sketch_error_probability / 2], size=(trials, repetitions))
-    gaussian_noise = np.random.normal(size=(trials, repetitions), scale=np.sqrt(repetitions))
-    non_private_errors = np.median(cs_errors, axis = 1)
-    private_errors = np.median(cs_errors + gaussian_noise, axis = 1)
-    error_difference = np.abs(private_errors) - count_sketch_error_value
-    sns.kdeplot(error_difference, label=f"k={repetitions}")
-
-sns.kdeplot(standard_gaussian_noise, label=f"Std. Gaussian", linestyle="dotted")
-
-plt.title(f'Additional error over Countsketch in a high variance setting')
-plt.xlim(-20,5)
-plt.legend()
-if trials >= 1000000:
-    plt.savefig('high-variance-setting.pdf')
-plt.show(block=False)
-
-
-
 # Failure probability of CountSketch with k repetitions
 
 def binomial(k, i):
@@ -108,6 +87,7 @@ plt.show(block=False)
 
 
 # Sparse vectors
+
 load_factor = 1. # Expected number of nonzero entries in each bucket
 entry_size = 10
 
@@ -128,7 +108,7 @@ if trials >= 1000000:
 plt.show(block=False)
 
 
-# Real-world examples
+# Error distribution on world cities vectors
 
 filename = "world_cities_population_simplemaps.txt"
 f = open(filename, "r")
@@ -170,3 +150,56 @@ xlimit = 300000
 kb_pairs = [ (1,10000), (3,10000), (5,10000), (9,10000), (19,10000) ]
 city_sketch_figure(cities_trials, kb_pairs, xlimit, noise_scale)
 
+
+# Cumulative noise distribution on two market basked datasets
+
+# Download data if needed:
+for filename in ['kosarak.dat.gz', 'retail.dat.gz']:
+    if not os.path.isfile(filename):
+        response = requests.get("http://fimi.uantwerpen.be/data/" + filename)
+        open(filename, "wb").write(response.content)
+
+repetitions = 5
+
+for (filename, countsketch_table_size, max_basket_size) in [("retail",500,30),("kosarak",1000, 100)]:
+    plt.figure()
+    skipped = 0
+    f = gzip.open(filename+".dat.gz", "rt")
+    item_counts = {}
+    for line in f:
+        if line.strip() != '':
+            basket = line.strip().split(" ")
+            if len(basket) > max_basket_size: #clip to maximum basket size
+                skipped += len(basket) - max_basket_size
+                basket = basket[:max_basket_size]
+            for x in basket:
+                item_counts[x] = item_counts.get(x, 0) + 1
+    
+    print(f'{filename}: {len(item_counts)} distinct elements, {sum([item_counts[x] for x in item_counts])} total elements, {skipped} skipped')
+    item_counts = np.array(sorted([ item_counts[x] for x in item_counts ]))
+    input_size = len(item_counts)
+    cp = 1 / countsketch_table_size # collision probability
+    
+    cs_errors = []
+    for _ in range(repetitions):
+        data = np.random.choice([-1, 1], p=[1/2, 1/2], size=input_size)
+        row = np.random.randint(0, countsketch_table_size, size=input_size)
+        col = np.array(range(input_size))
+        cs_matrix = coo_matrix((data, (row, col)), shape=(countsketch_table_size, input_size))
+        cs_errors.append(cs_matrix @ item_counts)
+    
+    cs_errors = np.array(cs_errors)
+    non_private_errors = np.median(cs_errors, axis = 0)
+    gaussian_noise = np.random.normal(size=(repetitions, countsketch_table_size), scale=max_basket_size*noise_scaling_factor*np.sqrt(repetitions))
+    # Noise scaled with maximum basket size
+    private_errors = np.median(cs_errors + gaussian_noise, axis = 0)
+    error_difference = np.abs(private_errors) - np.abs(non_private_errors)
+    sns.ecdfplot(private_errors, label=f"private CS, $\epsilon$={epsilon}, $\delta=${delta}")
+    splot = sns.ecdfplot(non_private_errors, label=f"standard CS")
+    splot.set_xlabel("Estimation error (up to)")
+
+    plt.title(f'Estimation error CDF {filename} (max basket size {max_basket_size}), k={repetitions}, b={countsketch_table_size}')
+    plt.xlim(-np.percentile(private_errors,98),np.percentile(private_errors,98))
+    plt.legend()
+    plt.savefig(f'cdf-{filename}-{repetitions}-{countsketch_table_size}.pdf')
+    plt.show(block=False)
